1d01a66b3e4d67673c1f5f5ffd65a20a651fb8bb
ripassorker2 <ripassorkar08@gmail.com>
2024-03-03 00:10:43 +0600

    done accomodatrion page design

--- src/app/pages/Home/Accomodation.jsx
+++ src/app/pages/Home/Accomodation.jsx
@@ -1,10 +1,81 @@
-import React from "react";
-
+import Image from "next/image";
+import img1 from "../../../assets/accomodation1.jpg";
+import img2 from "../../../assets/accomodation2.jpg";
+import {FaArrowRight} from "react-icons/fa";
 const Accomodation = () => {
     return (
         <div className="container">
-            <div className="mt-20">
-                <h1>Accomodations</h1>
+            <div className="md:mt-32 mt-20">
+                <div className="grid md:grid-cols-2 gap-10">
+                    <div>
+                        <Image
+                            src={img1}
+                            alt=""
+                            className="rounded object-contain"
+                        />
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div>
+                            <h2 className="heading">
+                                <span>Staff Accommodation</span>
+                            </h2>
+                            <h3 className="md:text-4xl text-xl md:my-4 my-2">
+                                Making it easier for all.
+                            </h3>
+                            <p className="desc">
+                                It doesn’t matter if you are an individual
+                                worker, a Company or a Student on placement at a
+                                hospital, we are here to help you with your
+                                stay. Browse our available properties in the
+                                location you desire or contact us to help with
+                                your requirements.
+                            </p>
+                            <button className="btn-secondary mt-4 inline-flex items-center md:py-4 group  ">
+                                Get a Quote{" "}
+                                <FaArrowRight className="ml-2 group-hover:ml-4 duration-300" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid md:grid-cols-2 gap-10 mt-20">
+                    <div className="block md:hidden">
+                        <Image
+                            src={img2}
+                            alt=""
+                            className="rounded object-contain"
+                        />
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div>
+                            <h2 className="heading">
+                                <span>Company Accommodation</span>
+                            </h2>
+                            <h3 className="md:text-4xl text-xl md:my-4 my-2">
+                                Long-term bookings.
+                            </h3>
+                            <p className="desc">
+                                If you are a company looking for a long-term
+                                stay of 4 months+, many of our hosts offer a
+                                discount on these types of bookings. If this
+                                doesn’t show in their listing, please fill in
+                                the enquiry form below and we will find you
+                                suitable accommodation and ensure your
+                                requirements are met.
+                            </p>
+                            <button className="btn-secondary mt-4 inline-flex items-center md:py-4 group  ">
+                                Get a Quote{" "}
+                                <FaArrowRight className="ml-2 group-hover:ml-4 duration-300" />
+                            </button>
+                        </div>
+                    </div>{" "}
+                    <div className="md:block hidden">
+                        <Image
+                            src={img2}
+                            alt=""
+                            className="rounded object-contain"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/app/pages/Home/Banner.jsx
+++ src/app/pages/Home/Banner.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import banner from "../../../assets/baner.png";
 import Link from "next/link";
+import {FaArrowRight} from "react-icons/fa";
 
 export const Banner = () => {
     return (
-        <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
-            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+        <div className="relative flex  py-16 lg:pt-0 flex-col lg:pb-0">
+            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-[59%] lg:max-w-full lg:absolute xl:px-0">
                 <svg
                     className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
                     viewBox="0 0 100 100"
@@ -14,20 +15,20 @@ export const Banner = () => {
                     <path d="M50 0H100L50 100H0L50 0Z" />
                 </svg>
                 <Image
-                    className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+                    className="object-cover bg-contain w-full  rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-[90%]"
                     src={banner}
                     alt="banner-image"
                 />
             </div>
-            <div className="relative flex flex-col items-start w-full max-w-xl container">
-                <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-                    <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl sm:leading-none">
+            <div className="relative flex flex-col items-start   container">
+                <div className="md:mb-16 lg:my-40 lg:max-w-xl lg:pr-5">
+                    <h2 className="mb-5 mt-6 md:mt-0 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl sm:leading-none">
                         Find Snug <br className="hidden md:block" />{" "}
                         <span> Accommodation</span> or{" "}
                         <br className="hidden md:block" /> Become a{" "}
                         <span>Snug Host</span>
                     </h2>
-                    <p className="pr-5 mb-5 text-base text-gray-900 ">
+                    <p className="pr-5 mb-5 text-base text-gray-800 ">
                         Discover suitable accommodation or showcase your
                         property for business professionals to easily find their
                         ideal stay.
@@ -50,12 +51,18 @@ export const Banner = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex items-center space-x-3 text-center">
-                        <Link href="" className="btn-secondary md:py-4 ">
-                            Find Accommodation
+                    <div className="flex items-center md:flex-row flex-col md:space-x-3 text-center">
+                        <Link
+                            href=""
+                            className="btn-secondary mt-4  md:py-4 group md:w-auto w-full ">
+                            Accommodation{" "}
+                            <FaArrowRight className="ml-2 group-hover:ml-4 duration-300" />
                         </Link>
-                        <Link href="" className="btn-secondary md:py-4">
-                            Become A Host
+                        <Link
+                            href=""
+                            className="btn-secondary mt-4  md:py-4 group md:w-auto w-full ">
+                            Become A Host{" "}
+                            <FaArrowRight className="ml-2 group-hover:ml-4 duration-300 " />
                         </Link>
                     </div>
                 </div>
